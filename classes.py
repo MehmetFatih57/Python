e0fdf902fc269130494509dacfc144b1a7436df8
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2024-03-27 10:28:08 +0300

    27/03/2024

--- classes.py
+++ classes.py
@@ -1,22 +1,14 @@
 class Student:
-    #attribute , property , field
-    name = 'Mehmet Fatih'
-    age = 29
+    def init(self, name, age) -> None:
+        self.name = name
+        self.age = age
+        print("Yapıcı blok çalıştı")
 
-    def __init__(self) -> None:
-        self.name
-        self.age
-        print("Yapici blok calisti")
+    def study(self):
+        print(f"{self.name} is studying...")
 
-def study(self):
-        print(f"{self.name}is Studying...")
+student1 = Student("Mehmet" , 25)
+#student1.study()        
 
-student1 = Student()
-student1.age = 25
-student1.name = "Fatih"
-student1.study()        
-
-student2 = Student()
-student2.age = 25
-student2.name = "Murat"
-student2.study()  
+student2 = Student("Fatih" , 25)
+#student2.study()  
