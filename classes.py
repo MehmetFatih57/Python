c951f5a3e2a1d267fe44015e3ff2f33ebb534591
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2024-03-27 10:55:35 +0300

    27/03/2024

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class Student:
-    def init(self, name, age) -> None:
+    def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
         print("Yapıcı blok çalıştı")
@@ -7,8 +7,8 @@ class Student:
     def study(self):
         print(f"{self.name} is studying...")
 
-student1 = Student("Mehmet" , 25)
-#student1.study()        
+student1 = Student("Mehmet", 25)
+student1.study()
 
-student2 = Student("Fatih" , 25)
-#student2.study()  
+student2 = Student("Fatih", 25)
+student2.study()
