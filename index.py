e79547501751d99a1854b6a741a02a53a6927f8a
Mehmet Fatih YILDIRIM <mehmet.fatihh57@gmail.com>
2024-03-27 11:54:02 +0300

    27/03/2024

--- index.py
+++ index.py
@@ -1,11 +1,7 @@
 #import classes #=> tum classes modulunu import eder
 from classes import Student
 import random
-import openpyxl
-
-
-
-
+#import openpyxl
 
 student1 = Student("Fatih" , 25)
 student1.study()        
@@ -13,4 +9,4 @@ student1.study()
 student2 = Student("Murat" , 25)
 student2.study()  
 
-print(random.random{})
+print(random.random())
